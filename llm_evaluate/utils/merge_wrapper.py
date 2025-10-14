a8480d040944b26a265b0dcb6e44bfde8d8ba869
dgme-syz <syz1729990469@163.com>
2025-10-14 18:06:46 +0800

    update for MT task

--- llm_evaluate/utils/merge_wrapper.py
+++ llm_evaluate/utils/merge_wrapper.py
@@ -3,41 +3,48 @@ from typing import Sequence, Literal
 from .metric.abstract import Metric
 
 
-def decorator(obj: Metric, merge_strategy: Literal["avg", "pass"] = "pass"):
+def decorator(obj: Metric, merge_strategy: Literal["avg", "pass", "total"] = "pass"):
 
     def wrapper(
         responses: Sequence[Sequence[str]], 
         answers: Sequence[str], 
         extra_infos: Sequence[dict], 
     ):
-        m = len(responses)
-        scores = [[] for _ in range(m)]
-        results = []
+        ret = {}
 
-        for j in range(m):  
-            row_responses = responses[j]
-            scores[j] = obj(row_responses, [answers[j]] * len(row_responses), extra_infos)["score"]
-
-        detailed_results = scores
-        n = len(responses[0])
-        if merge_strategy == "pass":
-            results = [max(x) for x in scores]
-        elif merge_strategy == "avg":
-            results = [sum(x) / len(x) if len(x) else None for x in scores]
+        if merge_strategy == "total":
+            all_responses = [resp for resps in responses for resp in resps]
+            all_answers = [ans for ans, res in zip(answers, responses) for _ in res]
+            results = obj(all_responses, all_answers, extra_infos)["score"]
         else:
-            raise NotImplementedError(
-                f"Please select merge_strategy from ['avg', 'pass']."
-                f"But got {merge_strategy}."
-            )    
-        
-        ret = {"score": results}
+            m = len(responses)
+            scores = [[] for _ in range(m)]
+            results = []
 
-        ret.update({
-            "extra_dict": {
-                "pass@1_score": [x[0] if len(x) else None for x in scores],
-                "detail_results": detailed_results,
-            }
-        })
+            for j in range(m):  
+                row_responses = responses[j]
+                scores[j] = obj(row_responses, [answers[j]] * len(row_responses), extra_infos)["score"]
+
+            detailed_results = scores
+            n = len(responses[0])
+            if merge_strategy == "pass":
+                results = [max(x) for x in scores]
+            elif merge_strategy == "avg":
+                results = [sum(x) / len(x) if len(x) else None for x in scores]
+            else:
+                raise NotImplementedError(
+                    f"Please select merge_strategy from ['avg', 'pass', 'total']."
+                    f"But got {merge_strategy}."
+                )    
+            
+            ret.update({
+                "extra_dict": {
+                    "pass@1_score": [x[0] if len(x) else None for x in scores],
+                    "detail_results": detailed_results,
+                }
+            })
+            
+        ret.update({"score": results})
         return ret
 
     return wrapper
